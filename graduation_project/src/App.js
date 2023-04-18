34abaa34d8b3fc6af404509d58fd41fc804a29e4
youssefAamer1710 <79087408+youssefAamer1710@users.noreply.github.com>
2023-04-18 14:40:03 +0200

    just committing

--- graduation_project/src/App.js
+++ graduation_project/src/App.js
@@ -14,7 +14,7 @@ function App() {
 		<div className="App">
 			<Nav />
 			<SideNav />
-			<Subjects />
+			<DepAdmin />
 		</div>
 	);
 }
--- graduation_project/src/component/Subjects.js
+++ graduation_project/src/component/Subjects.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import EventNoteIcon from "@mui/icons-material/EventNote";
